f240f3daff5c5750c6ae8b22596b121ea74e8530
Dakayao Aristo <da.aristo.y@gmail.com>
2025-06-15 11:55:09 +0200

    Auto-committed on 15/06/2025

--- src/Controllers/SettingsController.php
+++ src/Controllers/SettingsController.php
@@ -6,6 +6,9 @@ use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use SettingsEditor\Helpers\Settings;
 
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\File;
+
 class SettingsController extends Controller
 {
     public function edit()
@@ -20,5 +23,21 @@ class SettingsController extends Controller
         }
 
         return redirect()->back()->with('success', 'Paramètres mis à jour.');
+    }
+
+
+    public function robots_txt()
+    {
+        $mainPath = public_path('robots.txt');
+        $modulePath = public_path('vendor/settings-editor/robots.txt');
+
+        $mainContent = File::exists($mainPath) ? File::get($mainPath) : '';
+        $moduleContent = File::exists($modulePath) ? File::get($modulePath) : '';
+
+        $merged = trim($mainContent . "\n\n" . $moduleContent);
+
+        return Response::make($merged, 200, [
+            'Content-Type' => 'text/plain',
+        ]);
     }
 }
--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use SettingsEditor\Controllers\SettingsController;
 
 Route::middleware(['web'])->group(function () {
+    Route::get('/robots.txt', [SettingsController::class, 'robots_txt']);
+    
     Route::get('/settings-editor', [SettingsController::class, 'edit'])->name('torskint.settings_editor');
     Route::post('/settings-editor', [SettingsController::class, 'update'])->name('torskint.settings_editor.post');
 });
