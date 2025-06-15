daab6cae0b661f9d6d11b6b09a22a88f71855363
Dakayao Aristo <da.aristo.y@gmail.com>
2025-06-15 14:10:44 +0200

    Auto-committed on 15/06/2025

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -2,17 +2,17 @@
 
 @section('content')
     <div class="col-11 col-sm-9 col-md-7 col-lg-5 col-xl-4 m-auto py-5">
-        <form id="loginForm" method="post" action="{{ route('torskint.settings_editor.login.post') }}">
+        <form id="loginForm" autocomplete="nope" method="post" action="{{ route('torskint.settings_editor.login.post') }}">
             @csrf
 
             <x-torskint-settings-editor-alert />
             
             <div class="vertical-input-group">
                 <div class="input-group">
-                    <input type="email" class="form-control" id="emailAddress" required placeholder="Email or Username">
+                    <input type="email" autocomplete="nope" name="email" class="form-control" id="emailAddress" required placeholder="Adresse Email">
                 </div>
                 <div class="input-group">
-                    <input type="password" class="form-control" id="loginPassword" required placeholder="Password">
+                    <input type="password" autocomplete="nope" name="password" class="form-control" id="loginPassword" required placeholder="Mot de passe">
                 </div>
             </div>
             <div class="d-grid my-4">
