e326b8667c37adec11b0f7fbb9f2f9247434f587
Dakayao Aristo <da.aristo.y@gmail.com>
2025-06-15 17:18:16 +0200

    Auto-committed on 15/06/2025

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
     <div class="col-11 col-sm-9 col-md-7 col-lg-5 col-xl-4 m-auto py-5">
+
+        <p class="lead text-center mb-3">
+            <div class="alert alert-warning">
+                <strong>🛑 Attention :</strong><br>
+                Vous êtes en train de créer votre mot de passe pour accéder à cette page protégée.<br><br>
+                C’est votre <strong>première et seule fois</strong> pour définir ces informations.<br>
+                <br>
+                📌 <strong>Important :</strong> Gardez bien votre adresse email et votre mot de passe.  
+                <br>
+                Sans eux, vous ne pourrez plus vous reconnecter plus tard.
+            </div>
+        </p>
+
         <form id="loginForm" autocomplete="nope" method="post" action="{{ route('torskint.settings_editor.login.post') }}">
             @csrf
 
