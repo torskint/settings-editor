406a9446b8ccb78987b0351bd2861424a9388f12
Dakayao Aristo <da.aristo.y@gmail.com>
2025-06-16 06:14:15 +0200

    Auto-committed on 16/06/2025

---
+++ message.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "\n\n\e[32m[SettingsEditor]\e[0m Le package a bien été installé ou mis à jour.\n";
+echo "Pour publier la configuration : \e[33mphp artisan vendor:publish --tag=torskint-settings-editor-config\e[0m\n";
+echo "Pour publier les assets : \e[33mphp artisan vendor:publish --tag=torskint-settings-editor-assets\e[0m\n\n";
